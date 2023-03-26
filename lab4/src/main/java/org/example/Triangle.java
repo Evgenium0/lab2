32268f396f20b1855ff0e63472a00722e20ad95f
Evgenium0 <126943590+Evgenium0@users.noreply.github.com>
2023-03-26 19:53:58 +0300

    Add files via upload

---
+++ lab4/src/main/java/org/example/Triangle.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Triangle extends Figure {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    @Override
+    public double getArea() {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    @Override
+    public double gerPerimeter() {
+        return a + b + c;
+    }
+}
