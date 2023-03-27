acd6f9439cdf3a34539ebb7739460c825a83baf2
Evgenium0 <126943590+Evgenium0@users.noreply.github.com>
2023-03-27 14:27:09 +0300

    Delete lab4 directory

--- lab4/src/main/java/org/example/Figure.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public abstract class Figure {
-    //Общие свойства и методы для всех геометрических фигур
-    public  abstract  double getArea(); // Метод для вычисления площади фигуры
-    public  abstract  double gerPerimeter(); // Метод для вычисления периметра фигуры
-}
